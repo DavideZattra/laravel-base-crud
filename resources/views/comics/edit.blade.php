57d04262ebc0dc0113fffaeeed3c3620be8ff88b
DavideZattra <d.zattra@gmail.com>
2021-11-08 23:27:49 +0100

    create edit

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Io sono comics.edit</h1>
+
+    <a href="{{ route('comics.index')}}">Back to comics</a>
+
+    <form action="{{ route('comics.update', $comic )}}" method="POST">
+        
+        @method('PATCH')
+        @csrf
+
+        <div>
+
+            <input type="text" id="title" name="title" placeholder="inserisci il titolo" value="{{ $comic->title }}" required>
+            
+        </div>
+
+        <div>
+            
+            <input type="text" id="author" name="author" placeholder="inserisci l'autore" value="{{ $comic->author }}" required>
+            
+        </div>
+
+        <div>
+            
+            <input type="text" id="description" name="description" placeholder="inserisci la descrizione" value="{{ $comic->description }}" required>
+            
+        </div>
+
+        <div>
+            
+            <input type="text" id="url" name="url" placeholder="inserisci l'url della copertina" value="{{ $comic->url }}" required>
+            
+        </div>
+
+        <button type="reset">resetta i campi</button>
+        <button type="submit">inserisci il nuovo ospite</button>
+        
+    </form>
+@endsection
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,6 +8,7 @@
             <img src="{{$comic->url}}" width="200" alt="">
             <h3>{{$comic->title}}</h3>
             <p>{{$comic->description}}</p>
+            <a href="{{ route('comics.edit', ['comic'=>$comic])}}">Edit Comic</a>
             <a href="{{ route('comics.show', ['comic'=> $comic->id])}}">Read more</a>
 
         </div>
